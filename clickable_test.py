2859f9dc78e795eebf17326e25bc05eca943f8b1
Kazuto Mori <kazuto00112@gmail.com>
2018-07-11 14:58:51 +0900

    modified

--- clickable_test.py
+++ clickable_test.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
 import time
 
 driver = webdriver.Chrome(executable_path="/Users/kztmr/Downloads/chromedriver")
